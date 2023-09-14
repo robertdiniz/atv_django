0991d735b5ba459b7692d8d8e54c00c599780c98
Robert Diniz <94693689+robertdiniz@users.noreply.github.com>
2023-09-14 16:06:23 -0300

    finish

--- atv_django/asgi.py
+++ atv_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for app_1 project.
+ASGI config for atv_django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atv_django.settings')
 
 application = get_asgi_application()
